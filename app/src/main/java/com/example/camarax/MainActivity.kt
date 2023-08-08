44c2b100d1881c5aa742dfadb91a4e081fd892c1
SantiagoG <santigime972@gmail.com>
2023-08-08 15:26:49 -0300

    setup camera

--- app/src/main/java/com/example/camarax/MainActivity.kt
+++ app/src/main/java/com/example/camarax/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.camarax
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraInfo
+import androidx.camera.core.ImageCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import com.example.camarax.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val audioManager: AudioManager by lazy {
+        getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    }
+    private var mediaPlayer: MediaPlayer? = null
+    private var systemVolume: Int = 0
+    private val TAG = MainActivity::class.java.simpleName
+    private var isDisplayingTimer = false
+    private var isLensFacingBack: Boolean = true
+    private var thumbnailImageUri: Uri? = null
+    private var imageCapture: ImageCapture? = null
+    private var cameraControl: CameraControl? = null
+    private var cameraInfo: CameraInfo? = null
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/camarax/extension/ViewExt.kt
@@ -0,0 +1,10 @@
+package com.example.camarax.extension
+
+import android.view.View
+
+fun View.toVisible(){
+    visibility = View.VISIBLE
+}
+fun View.ToGone(){
+    visibility = View.GONE
+}
